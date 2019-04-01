90f009bfd7c5fc12c256466ffdac601036372e8c
Jonathan Mainguy <jon@soh.re>
2019-04-01 15:32:51 -0400

    you suck at bash

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Give us a pretty username
-okd-uid$(id | awk -F'd=' '{print $2}' | awk '{print $1}')
-sed -i "s/UID/${okd-uid}/g" /etc/passwd
+okd_uid=$(id | awk -F'd=' '{print $2}' | awk '{print $1}')
+sed -i "s/UID/$okd_uid/g" /etc/passwd
 sed -i "s/GID/0/g" /etc/passwd
-# keep a process running so container doesnt die
-tail -f /opt/run.sh
